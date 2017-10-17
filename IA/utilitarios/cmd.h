a73292b4cd0a110c16b8a0465b5b77fc694fbeff
cleissonbarbosa <32576001+cleissonbarbosa@users.noreply.github.com>
2017-10-17 02:09:44 -0200

    Update cmd.h

--- IA/utilitarios/cmd.h
+++ IA/utilitarios/cmd.h
@@ -1,22 +1,29 @@
 
-
-
 char cmd(char comando[30], char argumento[30]){
+	  //argumento = "start cd c://";
      
-     //while(stricmp(comando,"sair")!=0){
-     /*                                 
+     while(stricmp(argumento,"exit")!=0){
+                                      
      printf(":> ");
      gets(comando);
-     */
-     //if(stricmp(comando,"cmd")==0)
-          system("cmd");
-      /*  
-     else if(stricmp(comando,"ping")==0)
-     sprintf(comando,"ls %s",argumento); //passando um argumento para a função system
-     system(comando);   
-          
-     } */
      
-
+	    //system(argumento);         
+        
+     //else if(stricmp(comando,"ping")==0)
+     //sprintf(comando,"ls %s",argumento); //passando um argumento para a funÃ§Ã£o system
+     if(stricmp(comando,"exit")==0){
+        argumento = "echo Digite sair para voltar ao inicio";
+		system(argumento);
+	 }
+	 else if(stricmp(comando,"sair")==0){
+     	argumento = "exit";
+	 }
+	 else{
+	 	 system(comando); 
+	 }
+	 
+	 fflush(stdin);
+     
+     }
 
 }
